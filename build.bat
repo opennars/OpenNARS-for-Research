0992e4721f125cfce869c7eb69ca78c060146536
jeanmarc.vanel@gmail.com <jeanmarc.vanel@gmail.com@91dfdad4-c543-0410-b26a-7d79dded8189>
2013-03-28 15:06:20 +0000

    - update build.bat - add use_cases/pattern_matching1 (Pattern Recognition In NARS - Matthew Zaikowski) - update NARS 1.5.2 snapshot nars-dist/NARS.jar

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@ REM Very simple build script
 
 DEL classes
 mkdir classes
-javac -d classes -Xlint:unchecked nars\*\*.java
+javac -d classes -Xlint:unchecked nars_gui\src\main\java\*\*\*.java nars_core_java\src\main\java\*\*\*.java
 echo 'Main-Class: nars.main.NARS' > manifest.txt
 jar cvfm NARS.jar manifest.txt -C classes . 
 DEL manifest.txt
@@ -11,3 +11,5 @@ echo 'You can now launch:'
 echo 'java -jar NARS.jar'
 echo or
 echo 'java -jar NARS.jar nars-dist/Examples/Example-NAL1-in.txt'
+echo or
+echo 'java -cp NARS.jar nars.main_nogui.NARSBatch  nars-dist/Examples/Example-NAL1-in.txt'
